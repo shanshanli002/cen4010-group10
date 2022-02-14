bb32d445248a0fdba1518bbe23ff43e9085ef5aa
Berlin <Berline_lucien@yahoo.com>
2022-02-14 09:38:42 -0500

    registerPage on views

--- group10/profileManage/views.py
+++ group10/profileManage/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import *
+from .forms import OrderForm, CreateUserForm
+from .filters import OrderFilter
 import pyrebase
 # Create your views here.
 
@@ -18,17 +28,32 @@ firebase=pyrebase.initialize_app(config)
 authe = firebase.auth()
 database=firebase.database()
 
-def index(request):
+#def index(request):
     #accessing our firebase data and storing it in a variable
-    email = database.child('data').child('email').get().val()
-    phone = database.child('data').child('phone').get().val()
-    userName = database.child('data').child('username').get().val()
+    #email = database.child('data').child('email').get().val()
+    #phone = database.child('data').child('phone').get().val()
+    #userName = database.child('data').child('username').get().val()
 
 
-    context = {
-        'email':email,
-        'phone':phone,
-        'userName':userName
-    }
+    #context = {
+       # 'email':email,
+        #'phone':phone,
+       # 'userName':userName
+    #}
+def registerPage(request):
+    	  if request.user.is_authenticated:
+		            return redirect('home')
+	      else:
+		            form = CreateUserForm()
+		            if request.method == 'POST':
+			                  form = CreateUserForm(request.POST)
+			                  if form.is_valid():
+				                        form.save()
+				                        user = form.cleaned_data.get('username')
+				                        messages.success(request, 'Account was created for ' + user)
 
-    return render(request, 'index.html', context)
+				                        return redirect('login')
+			
+
+		              context = {'form':form}
+		              return render(request, 'templates/register.html', context)
