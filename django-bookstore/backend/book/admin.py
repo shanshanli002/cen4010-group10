cf182dc7bf2901c1edf2d70e672a458642f61627
ShanShan Li <63330376+shanshanli002@users.noreply.github.com>
2022-02-23 13:12:58 -0600

    changed admin display

--- django-bookstore/backend/book/admin.py
+++ django-bookstore/backend/book/admin.py
@@ -4,7 +4,8 @@ from .models import *
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
-    list_display =('Title','Author','Genre','Book_Description','Price','Publisher','Year_Published','Copies_Sold','ISBN')
+    #display for admin/book/book/
+    list_display =('ISBN','Title','Author','Genre','Book_Description','Price','Publisher','Year_Published','Copies_Sold')
     
     
 admin.site.register(Book, BookAdmin)
