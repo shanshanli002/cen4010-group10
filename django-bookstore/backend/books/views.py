24aeed9cc4d8027191ebc6313c01c633835ee7da
ShanShan Li <63330376+shanshanli002@users.noreply.github.com>
2022-02-23 18:10:32 -0600

    template displays DB data

--- django-bookstore/backend/books/views.py
+++ django-bookstore/backend/books/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 # Create your views here.
 def ISBN_Search(request):
     """client view to search for book to view book details do not need model"""
    #without template ------     return HttpResponse("<h1> Hello World </h1>")
-    return render (request, "ISBN_Search.html")
+    # var books = use name of model to retrieve from DB
+    books = Book.objects.all()
+    return render (request, "ISBN_Search.html", {'all_books': books})
 
 def Author_Books(request):
     """client view to search for books associated with a specific author do not need model"""
-    return render(request, "Author_Books.html") 
+    authors = Author.objects.all()
+    return render(request, "Author_Books.html", {'all_authors': authors}) 
