19e7b42bddf20957a61b0e93c87496da355cb58c
Berlin <Berline_lucien@yahoo.com>
2022-03-20 12:29:50 -0400

    credit card model

--- django-bookstore/backend/payment/models.py
+++ django-bookstore/backend/payment/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-#from users.models import CardNumberField, CardExpiryField, SecurityCodeField
+from users.models import CardNumberField, CardExpiryField, SecurityCodeField
 #import stripe 
 
 
 
 #create Credit Card that belongs to a User 
+
+class CreditCard(models.Model):
+    cc_number = models.CharField(max_length = 100) 
+    cc_expiry = models.CharField(max_length =20 )  
+    cc_code = models.CharField(max_length=3)
+    username = models.CharField(max_length = 100)
 '''
 class CreditCard(models.Model):
     cc_number = CardNumberField(_('card number'))
--- django-bookstore/backend/payment/serializers.py
+++ django-bookstore/backend/payment/serializers.py
@@ -1,6 +1,10 @@
-'''
+from rest_framework import serializers
+from django.contrib.auth.models import CreditCard
+from .models import *
+
 class CreditCard(serializers.ModelSerializer):
     class Meta:
         model = CreditCard
         fields = ('id','cc_number','cc_expiry','cc_code','username')
-  '''  
+        
+    return CreditCard
