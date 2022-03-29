cb6741a1c4c8bcf0fa24f6bb9f071ef27603ddab
Berlin <Berline_lucien@yahoo.com>
2022-03-29 10:08:37 -0400

    fixing conflict issue

--- django-bookstore/backend/books/apps.py
+++ django-bookstore/backend/books/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
+class BookConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-<<<<<<< HEAD:django-bookstore/backend/users/apps.py
-    name = 'users'
-=======
     name = 'books'
->>>>>>> main:django-bookstore/backend/books/apps.py
