173b88d0bffd45f0738de8b01dd457c0738b7b0c
Berlin <Berline_lucien@yahoo.com>
2022-03-29 09:44:17 -0400

    adding book files

--- django-bookstore/backend/backend/urls.py
+++ django-bookstore/backend/backend/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path, include
 from users import views
 #from payment import views
@@ -22,28 +21,23 @@ from users import views
 from users.views import *
 
 
-=======
 from django.urls import path
 from django.urls import include
 from books import views
 from books.views import *
->>>>>>> main
 urlpatterns = [
     #regular views for the django app
     path('', views.homepage),
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('users/',views.List_All_Users),
     #path('usersDetail/<str:username>/',views.User_Detail),
     #path('users/',views.Update_User),
     #path('users/',views.Delete_User),
     
-=======
     path('allbooks/', views.all_books),
     path('allauthors/', views.all_authors),
     #api views for requests get and post 
     path('books/', views.book_list),
     path('authors/', views.author_list),
     path('books/<int:ISBN>/', views.book_detail)
->>>>>>> main
 ]
