e090c3156e17b5d5f50aa96e76ed5f2abc578025
Berlin <Berline_lucien@yahoo.com>
2022-03-29 10:12:36 -0400

    cleaning up page

--- django-bookstore/backend/users/views.py
+++ django-bookstore/backend/users/views.py
@@ -10,38 +10,7 @@ from rest_framework.parsers import JSONParser
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.csrf import csrf_exempt
 
-#import stripe
-#from knox.models import AuthToken
 
-
-
-#from django.contrib.auth.forms import UserCreationForm
-#from .forms import UserRegisterForm
-#from django.contrib import messages
-#from django.contrib.auth.decorators import login_required
-
-
-#def home(request):
- #   return render(request, 'users/home.html')
-
-
-#def register(request):
-   # if request.method == "POST":
-   #     form = UserRegisterForm(request.POST)
-   #     if form.is_valid():
-   #         form.save()
-   #         username = form.cleaned_data.get('username')
-   #         messages.success(request, f'Hi {username}, your account was created successfully')
-   #         return redirect('home')
-    #else:
-    #    form = UserRegisterForm()
-
-    #return render(request, 'users/register.html', {'form': form})
-
-
-#@login_required()
-#def profile(request):
-    #return render(request, 'users/profile.html')
 
 #list all users 
 @csrf_exempt
