2a30d926e372ca2d880b63adfc2395658f40e213
Berlin <Berline_lucien@yahoo.com>
2022-04-02 00:16:48 -0400

    retrieving user by certain id

--- django-bookstore/backend/backend/urls.py
+++ django-bookstore/backend/backend/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 from django.urls import include
 from books import views
 from books.views import book_list, book_detail, author_list, all_books, all_authors
-from users.views import List_All_Users
+from users.views import List_All_Users, User_Detail
 
 urlpatterns = [
     #regular views for the django app
@@ -34,6 +34,7 @@ urlpatterns = [
     path('authors/', author_list),
     path('books/<int:ISBN>/', book_detail),
     #api views for profile management
-    path('users/',List_All_Users)
-    path('users/<string:username>/',User_Detail)
+    path('users/',List_All_Users),
+    path('users/<int:id>/',User_Detail)
+    
 ]
