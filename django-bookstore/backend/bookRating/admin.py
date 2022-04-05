1c74e566dad77f7380b4e98a62b465dec4f9f5a5
Franky Lopez <98270454+flope107@users.noreply.github.com>
2022-04-05 19:11:49 -0400

    update....

--- django-bookstore/backend/bookRating/admin.py
+++ django-bookstore/backend/bookRating/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from bookRating.models import Comment
+from .models import *
 
 # Register your models here.
 
 
-#class CommentAdmin(admin.ModelAdmin):
-#    list_display = ("book", "user", "rating","date","body") 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("book", "user", "body","rating","date") 
     #list_filter = ("date","body") # for filtering the comment by approval and date 
     #search_fields = ("users", "body", "rating")
 
 
-admin.site.register(Comment)
+admin.site.register(Comment,CommentAdmin)
--- django-bookstore/backend/bookRating/models.py
+++ django-bookstore/backend/bookRating/models.py
@@ -2,14 +2,11 @@ from django.db import models
 from books.models import Book
 from users.models import Users
 
-class Comment(models.Model): # comment model described as a model 
-    book = models.ForeignKey(Book, on_delete=models.CASCADE,default='Title') # foreign key from books used as a model 
-    user = models.ForeignKey(Users, on_delete=models.CASCADE,default='first_name') # foreign key from users used as a model 
-    #book = models.CharField(max_length=200)
-    #user = models.CharField(max_length = 100) 
-    body = models.TextField(max_length=500) # text field that will be used for the comment 
-    date = models.DateTimeField(auto_now_add=True) # the time at which user made the comment 
-    rating = models.CharField(max_length=10)
-
-    class Meta:
-        ordering = ['date']
+class Comment(models.Model):
+    #book = models.ForeignKey(Book, on_delete=models.CASCADE,default='Title') # foreign key from books used as a model 
+    #user = models.ForeignKey(Users, on_delete=models.CASCADE,default='first_name') # foreign key from users used as a model 
+    book = models.CharField(max_length = 500)
+    user = models.CharField(max_length = 500) 
+    body = models.CharField(max_length = 500) 
+    rating = models.CharField(max_length = 500)
+    date = models.DateField()
