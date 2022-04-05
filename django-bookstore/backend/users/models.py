a12e382c99d55b01b9af6939c526406a28093505
Berlin <Berline_lucien@yahoo.com>
2022-04-05 13:57:46 -0400

    users

--- django-bookstore/backend/users/models.py
+++ django-bookstore/backend/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
-    user =models.CharField(max_length=200,null=True)
+    user=models.CharField(max_length=200,null=True)
     name=models.CharField(max_length=200,null=True)
     email=models.CharField(max_length=200,null=True)
     password=models.CharField(max_length=200,null=True)
