e9241bb54b11ca842c0e9c21eefbb1a603ac47a2
Berlin <Berline_lucien@yahoo.com>
2022-04-08 16:29:03 -0400

    fixed misspelled model

--- django-bookstore/backend/users/admin.py
+++ django-bookstore/backend/users/admin.py
@@ -8,7 +8,7 @@ from .models import *
 
 class CustomerAdmin(admin.ModelAdmin):
     #display for admin/users/accounts
-    list_display=('id','user','name','email','address','password','card_info')
+    list_display=('id','username','name','email','address','password','card_info')
  
 
 admin.site.register(Customer)
--- django-bookstore/backend/users/serializers.py
+++ django-bookstore/backend/users/serializers.py
@@ -6,7 +6,7 @@ from .models import *
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ('id','user', 'name', 'email','address','password','card_info')
+        fields = ('id','username', 'name', 'email','address','password','card_info')
         extra_kwargs = {'password':{'write_only':True}}
 
  
