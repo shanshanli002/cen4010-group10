d2d9406c55321ad6148a8aee299569486b3f534f
Franky Lopez <98270454+flope107@users.noreply.github.com>
2022-04-08 21:26:03 -0400

    update i guess

--- django-bookstore/backend/bookRating/admin.py
+++ django-bookstore/backend/bookRating/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
-from .models import *
+from .models import Comment
 
 # Register your models here.
 
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ("book", "user", "body","rating","date") 
-    #list_filter = ("date","body") # for filtering the comment by approval and date 
-    #search_fields = ("users", "body", "rating")
-
-
-admin.site.register(Comment,CommentAdmin)
+admin.site.register(Comment)
