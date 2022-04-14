8936af8e69ce70019f84d45a85ccb714b5873cc6
MPLichtman <78001556+MPLichtman@users.noreply.github.com>
2022-04-14 15:14:28 -0400

    pls wrk

--- django-bookstore/backend/wishlist/views.py
+++ django-bookstore/backend/wishlist/views.py
@@ -1,8 +1,19 @@
 from curses.ascii import HT
-from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
+from books.models import Book, Author
 from rest_framework import generics
+from wishlist.models import wishlistenitity
+from wishlist.serializers import WishlistSerializer
+from rest_framework.views import APIView
 
+class WishlistView(APIView):
 
+    # view Wishlist
+    def get(request, pk=None):
+        ViewWishlist = wishlistenitity.objects.all()
+        if request.method == 'GET':
+            #serializer = WishlistSerializer(ViewWishlist, many=True)
+            return HttpResponse(ViewWishlist, status= 200)
+        else: 
+            return HttpResponse(ViewWishlist, status= 400)
