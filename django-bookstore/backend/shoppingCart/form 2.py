9dd5df7996e49d1e552068ea45344dad64e1278c
ShanShan Li <63330376+shanshanli002@users.noreply.github.com>
2022-04-14 04:04:07 -0500

    delete duplicates

--- django-bookstore/backend/shoppingCart/form 2.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from books.models import Book
-
-class shoppingCartForm(forms.Form):
-    book_pk = forms.IntegerField(min_value=1)
-    next_link = forms.CharField(max_length=240)
-   
