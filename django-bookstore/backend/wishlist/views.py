3b7d5f980195fa8d487790c2dcc656a6660003f6
Berlin <Berline_lucien@yahoo.com>
2022-04-14 17:57:36 -0400

    fixing conflict

--- django-bookstore/backend/wishlist/views.py
+++ django-bookstore/backend/wishlist/views.py
@@ -6,11 +6,31 @@ from rest_framework import generics
 from wishlist.models import wishlistenitity
 from wishlist.serializers import WishlistSerializer
 from rest_framework.views import APIView
+from django.views.decorators.csrf import csrf_exempt
+
 
 class WishlistView():
     # view Wishlist
-    def getWishList(request, pk=None):
+    def getWishList(request, pk = None):
         if request.method == 'GET':
             wishlistobjects = wishlistenitity.objects.all()
             serializer = WishlistSerializer(wishlistobjects, many=True)
             return JsonResponse(serializer.data, safe=False)
+
+    @csrf_exempt
+    def removefromwishlist(request, id):
+        if request.method == 'DELETE':
+            wishlistenitity.objects.filter(id = id).delete()
+            return HttpResponse(status = 204)
+
+    @csrf_exempt
+    def createwishlist(request):
+        if  request.method == 'POST':
+            data = JSONParser().parse(request)
+            serializer = WishlistSerializer(data=data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=201)
+
+            return JsonResponse(serializer.errors, status=400)
